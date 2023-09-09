ec11167b71e5d45e274cd81c5b73adfdbdc7263e
Lonten Technology <142191915+LontenTechnology@users.noreply.github.com>
2023-09-09 17:51:27 +0800

    Add files via upload

---
+++ Code/Lesson 24 Photocell/Photocell/Photocell.ino
@@ -0,0 +1,38 @@
+
+
+int lightPin = 0;
+int latchPin = 11;
+int clockPin = 9;
+int dataPin = 12;
+
+int leds = 0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);  
+  pinMode(clockPin, OUTPUT);
+}
+void updateShiftRegister()
+{
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, leds);
+   digitalWrite(latchPin, HIGH);
+}
+void loop() 
+{
+  int reading  = analogRead(lightPin);
+  int numLEDSLit = reading / 57;  //1023 / 9 / 2
+  Serial.println(numLEDSLit);
+  if (numLEDSLit > 8) 
+  numLEDSLit = 8;
+  leds = 0;   // no LEDs lit to start
+  for (int i = 0; i < numLEDSLit; i++)
+  {
+    leds = leds + (1 << i);  // sets the i'th bit
+  }
+  updateShiftRegister();
+}
+
+
